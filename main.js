0009e3c7c1f5821c37d8bcd25e3d8794a33a1daa
tomcoakes <tomcoakes@gmail.com>
2020-07-23 22:51:12 +0100

    convert to class

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
 
@@ -9,28 +8,33 @@ const random = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min
 }
 
-function Ball() {
-  this.x = random(0, width)
-  this.y = random(0, height)
-  this.velX = random(-7, 7)
-  this.velY = random(-7, 7)
-  this.colour = `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`
-  this.size = random(10, 60)
+class Ball {
+  constructor() {
+    this.x = random(0, width)
+    this.y = random(0, height)
+    this.velX = random(-7, 7)
+    this.velY = random(-7, 7)
+    this.colour = `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`
+    this.size = random(10, 60)
+  }
+  draw = draw
+  update = update
+  collisionDetection  = collisionDetection
 }
 
-Ball.prototype.draw = function() {
+const draw = function (){
   ctx.beginPath()
   ctx.fillStyle = this.colour
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI)
   ctx.fill()
 }
 
-Ball.prototype.update = function() {
-  if((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+const update = function (){
+  if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
     this.velX = -(this.velX)
   }
 
-  if((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+  if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
     this.velY = -(this.velY)
   }
 
@@ -38,15 +42,15 @@ Ball.prototype.update = function() {
   this.y += this.velY
 }
 
-Ball.prototype.collisionDetection = function() {
-  for(let i = 0; i < balls.length; i++) {
-    if( (!(this.x === balls[i].x && this.y === balls[i].y && this.velX === balls[i].velX && this.velY === balls[i].velY)) ) {
+const collisionDetection = function (){
+  for (let i = 0; i < balls.length; i++) {
+    if ((!(this.x === balls[i].x && this.y === balls[i].y && this.velX === balls[i].velX && this.velY === balls[i].velY))) {
       const dx = this.x - balls[i].x
       const dy = this.y - balls[i].y
       const distance = Math.sqrt(dx * dx + dy * dy)
 
       if (distance < this.size + balls[i].size) {
-        balls[i].color = this.colour = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
+        balls[i].color = this.colour = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
       }
     }
   }
@@ -54,7 +58,7 @@ Ball.prototype.collisionDetection = function() {
 
 const balls = []
 
-function loop() {
+const loop = () => {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)'
   ctx.fillRect(0, 0, width, height)
 
